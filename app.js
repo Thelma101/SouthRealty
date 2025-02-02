1a882f66e0cca3e5feaf7bd520a26cf8c09e2c46
Thelma101 <akpata.thelma@gmail.com>
2025-02-02 13:35:13 +0100

    handlebars config

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const app = express();
 const exhbs = require('express-handlebars');
 const PORT = process.env.PORT || 4321;
 
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
 // handlebars templating
 // view engine set up
-app.set()
+app.set('view engine', 'handlebars');
+app.engine('handlebars', exhbs({defaultLayout: 'main'}));
+
 
 
 // app.engine('handlebars', exhbs({defaultLayout: 'main'}));
