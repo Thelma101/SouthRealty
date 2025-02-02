4650015da7c5dc26ef756b9fc5f883f997ab18ec
Thelma101 <akpata.thelma@gmail.com>
2025-02-02 13:51:43 +0100

    handlebars reconfig

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
 const express = require('express');
 const app = express();
 const exhbs = require('express-handlebars');
+const path = require('path');
 const PORT = process.env.PORT || 4321;
-
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
 
 
 // handlebars templating
 // view engine set up
-app.set('view engine', 'handlebars');
-app.engine('handlebars', exhbs({defaultLayout: 'main'}));
+app.engine("hbs", exphbs.engine({
+    extname: ".hbs",
+    defaultLayout: "main",
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+        allowProtoMethodsByDefault: true
+    }
+}))
 
-
-
-// app.engine('handlebars', exhbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+app.set('view engine', 'hbs')
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")))
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req,res) => {
-    res.render('index')
-})
+    res.render('index', {home: true})
+});
 
+app.get('/', (req,res) => {
+    res.render('about', {home: true})
+})
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    
+    console.log(`Server is running on port ${PORT}`);  
 })
