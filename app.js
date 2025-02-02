365091160eb562adb98a781008c9130dbc3b206f
Thelma101 <akpata.thelma@gmail.com>
2025-02-02 13:16:54 +0100

    handlebars config

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const exhbs = require('express-handlebars');
+const PORT = process.env.PORT || 4321;
+
+app.engine('handlebars', exhbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req,res) => {
+    res.render('index')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    
+})
